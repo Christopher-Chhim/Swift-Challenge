e13cb8125017c40e2a7f56e7887a55d6a3157e21
Christopher-Chhim <christopher.chhim123@gmail.com>
2026-02-20 21:03:11 -0800

    Initial Commit

---
+++ CareerExplore/CareerExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerExploreApp.swift
+//  CareerExplore
+//
+//  Created by Christopher Chhim on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct CareerExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
